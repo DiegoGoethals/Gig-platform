2583c74292e9f538b602377b3089165e94df95b1
DiegoGoethals <goethalsdiego@gmail.com>
2024-11-14 17:35:07 +0100

    Made basics of all entities

---
+++ Gig.Platform.Core/Entities/Job.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gig.Platform.Core.Entities
+{
+    public class Job : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Salary { get; set; }
+        public Guid EmployerId { get; set; }
+        public ApplicationUser Employer { get; set; }
+        public ICollection<Application> Applications { get; set; }
+        public ICollection<Skill> Skills { get; set; }
+    }
+}
