3d923669949fe14577ad82f42c20f17922cf2b17
DiegoGoethals <goethalsdiego@gmail.com>
2024-11-21 15:49:49 +0100

    jwt gets saved and authentication is required before doing anything on the site #33

---
+++ Gig.Platform.Web/Components/Pages/SecurePageBase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components;
+using System.Security.Claims;
+
+namespace Gig.Platform.Web.Components.Pages
+{
+    public class SecurePageBase : ComponentBase
+    {
+        [Inject] protected AuthenticationStateProvider AuthStateProvider { get; set; }
+        [Inject] protected NavigationManager NavigationManager { get; set; }
+
+        protected ClaimsPrincipal User { get; private set; }
+        private bool _isNavigationHandled;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender && !_isNavigationHandled)
+            {
+                _isNavigationHandled = true;
+
+                var authState = await AuthStateProvider.GetAuthenticationStateAsync();
+                User = authState.User;
+
+                if (User.Identity == null || !User.Identity.IsAuthenticated)
+                {
+                    NavigationManager.NavigateTo("/auth");
+                }
+            }
+        }
+    }
+}
