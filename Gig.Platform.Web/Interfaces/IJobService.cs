6618b5576eb64be89117bac075cc6ea71aa79701
DiegoGoethals <goethalsdiego@gmail.com>
2024-11-22 14:37:20 +0100

    Made basic jobCard and basic homescreen #28

---
+++ Gig.Platform.Web/Interfaces/IJobService.cs
@@ -0,0 +1,7 @@
+﻿namespace Gig.Platform.Web.Interfaces
+{
+    public interface IJobService
+    {
+        Task<IEnumerable<JobResponseDto>> GetAllJobsForEmployeesAsync();
+    }
+}
---
+++ Gig.Platform.Web/Services/JobService.cs
@@ -0,0 +1,20 @@
+﻿using Gig.Platform.Web.Interfaces;
+
+namespace Gig.Platform.Web.Services
+{
+    public class JobService : IJobService
+    {
+        private readonly HttpClient _httpClient;
+
+        public JobService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<JobResponseDto>> GetAllJobsForEmployeesAsync()
+        {
+            var response = await _httpClient.GetAsync("api/jobs");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<IEnumerable<JobResponseDto>>();
+        }
+}
