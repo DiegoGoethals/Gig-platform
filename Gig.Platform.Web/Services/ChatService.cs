10f68687d77b6290491dfab64737ab28e0a8216b
DiegoGoethals <goethalsdiego@gmail.com>
2024-12-10 15:40:20 +0100

    Add functionality to see messages in chat

--- Gig.Platform.Web/Services/ChatService.cs
+++ Gig.Platform.Web/Services/ChatService.cs
@@ -12,5 +12,19 @@ namespace Gig.Platform.Web.Services
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadFromJsonAsync<IEnumerable<AccountResponseDto>>();
         }
+
+        public async Task<MessageResponseDto> SendMessageAsync(MessageRequestDto messageRequestDto)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/messages", messageRequestDto);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<MessageResponseDto>();
+        }
+
+        public async Task<IEnumerable<MessageResponseDto>> GetConversationAsync(Guid id1, Guid id2)
+        {
+            var response = await _httpClient.GetAsync($"api/messages/conversation/{id1}/{id2}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<IEnumerable<MessageResponseDto>>();
+        }
     }
 }
