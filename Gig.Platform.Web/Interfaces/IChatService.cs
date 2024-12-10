6aefe31da28876e9297054c0a1b9fdaf5605bb27
DiegoGoethals <goethalsdiego@gmail.com>
2024-12-10 15:20:38 +0100

    Get chat partners to display in sidebar

---
+++ Gig.Platform.Web/Interfaces/IChatService.cs
@@ -0,0 +1,7 @@
+﻿namespace Gig.Platform.Web.Interfaces
+{
+    public interface IChatService
+    {
+        Task<IEnumerable<AccountResponseDto>> GetChatPartnersAsync(Guid id);
+    }
+}
---
+++ Gig.Platform.Web/Services/ChatService.cs
@@ -0,0 +1,16 @@
+﻿using Gig.Platform.Web.Interfaces;
+
+namespace Gig.Platform.Web.Services
+{
+    public class ChatService(HttpClient httpClient) : IChatService
+    {
+        private readonly HttpClient _httpClient = httpClient;
+
+        public async Task<IEnumerable<AccountResponseDto>> GetChatPartnersAsync(Guid id)
+        {
+            var response = await _httpClient.GetAsync($"api/messages/partners/{id}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<IEnumerable<AccountResponseDto>>();
+        }
+    }
+}
