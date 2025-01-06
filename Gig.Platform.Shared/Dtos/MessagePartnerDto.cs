d3f4c65c0583a6640376d4a5079bd5f95c41ff58
DiegoGoethals <goethalsdiego@gmail.com>
2025-01-06 19:09:34 +0100

    Reworked chat partner logic to also include last message between partners

---
+++ Gig.Platform.Shared/Dtos/MessagePartnerDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gig.Platform.Shared.Dtos
+{
+    public class MessagePartnerDto
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public string LastMessage { get; set; }
+        public DateTime LastMessageDate { get; set; }
+    }
+}
